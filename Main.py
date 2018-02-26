7aa8723c0ae40cc16056d225dfb18d3b5144d843
idoprog <ido.gamer313@gmail.com>
2018-02-26 20:52:52 +0200

    Really basic stuff
    making the screen full creating escape exit and closing exit

--- Main.py
+++ Main.py
@@ -1,8 +1,20 @@
 import pygame
 pygame.init()
+screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN | pygame.NOFRAME, 16)
+pygame.display.toggle_fullscreen()
 
-while True:
-    print '1'
+finish = False
 
+while not finish:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        finish = True
 pygame.quit()
+
+
+
 
