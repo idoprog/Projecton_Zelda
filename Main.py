d7e086ddbb425b6b00291bcbb9719e86df6da362
idoprog <ido.gamer313@gmail.com>
2018-03-04 08:50:27 +0200

    One more step
    managed to choose part of an image and then scaling it to wanted size

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN | pygame.NOFRAME, 16)
-pygame.display.toggle_fullscreen()
+screen = pygame.display.set_mode((1920, 1080), pygame.NOFRAME | pygame.FULLSCREEN, 16)
 
+img = pygame.Surface.convert(pygame.image.load('level5.png'))
+img.set_clip(pygame.Rect(0, 0, 144, 81))
+printer = img.subsurface(img.get_clip())
+pygame.transform.scale(printer, (1920, 1080), screen)
+pygame.display.update()
 finish = False
 
 while not finish:
