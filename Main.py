4cca5a2de104002f2419e867fb302588fdb8c633
idoprog <ido.gamer313@gmail.com>
2018-04-11 23:47:21 +0300

    A Walking character
    I've implemented the walking animation and I also fixed some things and changed the logic a bit.
    Need to implement THE WALLS

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
-
-
 import pygame
 pygame.init()
 
 
 screen = pygame.display.set_mode((1280, 720), pygame.NOFRAME | pygame.FULLSCREEN, 32)
-
+steps = 5
 
 class rpg_board(pygame.Rect):
     def __init__(self, start_x=0, start_y=0):
@@ -16,62 +14,106 @@ class rpg_board(pygame.Rect):
         screen.blit(self.image, (0, 0), area=(self.left, self.top, self.width, self.height))
         pygame.display.update()
 
-    def blit_image(self):
+    def blit_board(self):
         screen.fill((255, 255, 255))
         screen.blit(self.image, (0, 0), area=(self.left, self.top, self.width, self.height))
 
-
-    def move_up(self):
-        if self.top - 10 == 0:
+    def move_up(self, steps):
+        if self.top - steps == 0:
             return
-        self.top = self.top-10
-        self.height = self.height -10
-        self.blit_image()
+        self.top = self.top - steps
+        self.height = self.height - steps
+        self.blit_board()
+        char.walk(1)
+        pygame.display.update()
 
-    def move_down(self):
-        if self.height + 10 == self.image_height:
+    def move_down(self, steps):
+        if self.height + steps == self.image_height:
             return
-        self.height = self.height + 10
-        self.top = self.top + 10
-        self.blit_image()
+        self.height = self.height + steps
+        self.top = self.top + steps
+        self.blit_board()
+        char.walk(3)
+        pygame.display.update()
 
-    def move_left(self):
-        if self.left - 10 == 0:
+    def move_left(self, steps):
+        if self.left - steps == 0:
             return
-        self.left = self.left - 10
-        self.width = self.width - 10
-        self.blit_image()
+        self.left = self.left - steps
+        self.width = self.width - steps
+        self.blit_board()
+        char.walk(4)
+        pygame.display.update()
 
-    def move_right(self):
-        if self.width + 10 == self.image_width:
+    def move_right(self, steps):
+        if self.width + steps == self.image_width:
             return
-        self.width = self.width + 10
-        self.left = self.left + 10
-        self.blit_image()
+        self.width = self.width + steps
+        self.left = self.left + steps
+        self.blit_board()
+        char.walk(2)
+        pygame.display.update()
 
 
-board = rpg_board(0, 0)
+board = rpg_board(130, 920)
 
 
 class main_character(rpg_board):
-    def __init__(self,start_x, start_y):
-        super(rpg_board, self).__init__(start_x, start_y, start_x + 80, start_y + 80)
+    def __init__(self, start_x, start_y):
+        super(rpg_board, self).__init__(start_x, start_y, start_x + 80, start_y + 80)  #going to the father of rpg_board (pygame.Rect())
+        self.direction = 1
+        self.moving_seq = 1
+    @staticmethod
+    def image_bliting(image_loc, pos_x, pos_y, angle):
+        screen.blit(pygame.transform.rotate(pygame.image.load(image_loc), angle),(pos_x, pos_y))
 
-    def image_bliting(self, image_loc, pos_x, pos_y):
-        screen.blit(pygame.image.load(image_loc),(pos_x, pos_y))
-
-    def breath(self, state):
-        board.blit_image()
-        if(state):
-            self.image_bliting('Textures/Skins/Main_Char/Animation/B/B1.png', self.left, self.top)
-            return False
+    def calc_angle(self):
+        if self.direction == 1:
+            return 0
+        elif self.direction == 2:
+            return 270
+        elif self.direction == 3:
+            return 180
         else:
-            self.image_bliting('Textures/Skins/Main_Char/Animation/B/B2.png', self.left, self.top)
-            return True
+            return 90
 
-char = main_character(600,320)
+    def breath(self):
+        board.blit_board()
+        state = False
+        while True:
+            if state:
+                self.image_bliting('Textures/Skins/Main_Char/Animation/B/B1.png', self.left, self.top, self.calc_angle())
+                state = False
+            else:
+                self.image_bliting('Textures/Skins/Main_Char/Animation/B/B2.png', self.left, self.top, self.calc_angle())
+                state = True
+            pygame.display.update()
+            for x in range(1000):
+                pygame.time.delay(1)
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        return
 
-state = True
+    def walk(self, d):
+        if self.direction == d:
+            self.image_bliting('Textures\Skins\Main_Char\Animation\W\W' + str(self.moving_seq) + '.png', self.left, self.top, self.calc_angle())
+        else:
+            self.moving_seq = 1
+            self.direction = d
+            self.image_bliting('Textures\Skins\Main_Char\Animation\W\W1.png', self.left, self.top, self.calc_angle())
+        self.moving_seq = self.moving_seq + 1
+        if self.moving_seq == 35:
+            self.moving_seq = 1
+        for x in range(20):
+            pygame.time.delay(1)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYUP:
+                    return
+
+
+char = main_character(600, 320)
+
+
 finish = False
 while not finish:
     for event in pygame.event.get():
@@ -82,16 +124,15 @@ while not finish:
     if keys[pygame.K_ESCAPE]:
         finish = True
     if keys[pygame.K_w]:
-        board.move_up()
+        board.move_up(steps)
     elif keys[pygame.K_s]:
-        board.move_down()
+        board.move_down(steps)
     elif keys[pygame.K_a]:
-        board.move_left()
+        board.move_left(steps)
     elif keys[pygame.K_d]:
-        board.move_right()
+        board.move_right(steps)
     else:
-        state = char.breath(state)
+        char.breath()
 
-    pygame.display.update()
-    pygame.time.delay(1000)
+
 pygame.quit()
--- Testing.py
+++ Testing.py
@@ -1,12 +1,22 @@
-class lol(object):
-    def __init__(self, noob, length):
-        self.noob = noob
-        self.length = length
+import pygame
+pygame.init()
 
 
+screen = pygame.display.set_mode((1280, 720), pygame.NOFRAME | pygame.FULLSCREEN, 32)
+image = pygame.image.load('Textures\Skins\Main_Char\MC_S.png')
 
+screen.blit(pygame.transform.rotate(image, 90), (100, 100))
+screen.blit(pygame.transform.rotate(image, 180), (500, 500))
+pygame.display.update()
+finish = False
+while not finish:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
 
+    keys = pygame.key.get_pressed()
+    print keys
+    if keys[pygame.K_ESCAPE]:
+        finish = True
 
-class lel(lol):
-    def __init__(self):
-        super(lel, self).__init__(3,5)
+pygame.quit()
